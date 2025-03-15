ca5418e1496862a713ad3de9574d01b088f4f7ca
hscottvo <56327086+hscottvo@users.noreply.github.com>
2025-03-15 09:14:26 -0700

    Update nix setup

--- flake.nix
+++ flake.nix
@@ -3,6 +3,24 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
+    devShells.aarch64-darwin =
+      let
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      in
+      {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            cargo-watch
+            lld
+            rustc
+            SDL
+            sdl3
+            wasm-pack
+          ];
+          shellHook = "exec zsh --login";
+        };
+      };
     devShells.x86_64-linux =
       let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
@@ -17,7 +35,9 @@
             SDL
             sdl3
             wasm-pack
+            zsh
           ];
+          shellHook = "exec zsh --login";
         };
       };
   };
--- shell.nix
+++ shell.nix
@@ -5,7 +5,32 @@ pkgs.mkShell {
     cargo-watch
     lld
     rustc
+    SDL
     sdl3
     wasm-pack
   ];
 }
+# { pkgs ? import <nixpkgs> { }, home-manager ? import <home-manager/nixpkgs> { } }:
+#
+# pkgs.mkShell
+# {
+#   buildInputs = with pkgs; [
+#     cargo
+#     cargo-watch
+#     lld
+#     rustc
+#     wasm-pack
+#   ] ++ (if pkgs.stdenv.isDarwin then [ ] else [ sdl3 ]);
+# }
+#
+#   home-manager.lib.homeManagerConfiguration
+# {
+#   pkgs = pkgs;
+#   modules = [{
+#     homebrew = {
+#       enable = true;
+#       brews = [ "sdl3" ];
+#     };
+#   }];
+# }
+#
