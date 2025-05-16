026651fb70ebfbc1f6100c0d20ac16dc754cda60
hscottvo <56327086+hscottvo@users.noreply.github.com>
2025-05-16 12:54:44 -0700

    add clippy-recommended changes

--- src/hardware/cartridge.rs
+++ src/hardware/cartridge.rs
@@ -1,23 +1,30 @@
-use std::ops::Index;
 use std::fs::read;
+use std::ops::Index;
+use tracing::debug;
 
 pub struct Cartridge {
     data: Vec<u8>,
 }
 
 impl Cartridge {
-    pub fn new(path: &str) -> Self {
-        let read_result = read(path);
+    /// # Errors
+    ///
+    /// Will return 'Err' if the the file is unable to be read
+    pub fn new(path: &str) -> std::io::Result<Self> {
+        let data = read(path)?;
 
-        let data = match read_result {
-            Ok(bytes) => bytes,
-            Err(e) =>  panic!("{:?}", e)//{println!("{:?}", e); Vec::new()}
-        };
-        println!("{:#04X?}", &data[0x0104..=0x0133]);
-
-        Cartridge{data: data}
+        debug!("Read {:#06X} bytes", data.len());
+        Ok(Cartridge { data })
     }
-    // pub fn 
+
+    #[must_use]
+    pub fn mbc_type(&self) -> &u8 {
+        &self.data[0x0147]
+    }
+    #[must_use]
+    pub fn data(&self) -> &Vec<u8> {
+        &self.data
+    }
 }
 
 impl Index<usize> for Cartridge {
@@ -31,15 +38,30 @@ impl Index<usize> for Cartridge {
 mod tests {
     use super::Cartridge;
 
-    #[test]
-    fn one() {
-        let cart = Cartridge::new("./roms/tetris.gb");
-        assert_ne!(cart.data, Vec::new());
-    }
+    // #[test]
+    // fn read_pokemon_red() {
+    //     let cart = Cartridge::new("./roms/pkmn-red.gb").unwrap();
+    //     assert_ne!(cart.data, Vec::new());
+    //     assert_eq!(*cart.mbc_type(), 0x13);
+    // }
+    //
+    // #[test]
+    // fn read_tetris() {
+    //     let cart = Cartridge::new("./roms/tetris.gb").unwrap();
+    //     assert_eq!(*cart.mbc_type(), 0x00);
+    //     assert_ne!(cart.data, Vec::new());
+    // }
+    //
+    // #[test]
+    // fn read_tloz() {
+    //     let cart = Cartridge::new("./roms/tloz.gb").unwrap();
+    //     assert_ne!(cart.data, Vec::new());
+    //     assert_eq!(*cart.mbc_type(), 0x03);
+    // }
 
     #[test]
-    #[should_panic]
+    #[should_panic = "No such file or directory"]
     fn dne() {
-        let _ = Cartridge::new("./roms/dne.gb");
+        let _ = Cartridge::new("./roms/dne.gb").unwrap();
     }
 }
